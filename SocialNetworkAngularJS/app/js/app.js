add23cfabb8ec7af2b05001788f8ee986abd4b3b
Tzezo <tzvetomir.krastev@gmail.com>
2015-05-27 20:42:19 +0300

    Login and register

---
+++ SocialNetworkAngularJS/app/js/app.js
@@ -0,0 +1,33 @@
+'use strict';
+
+var app = angular.module('app', ['ngRoute', 'ngResource', 'ui.bootstrap.pagination']);
+
+app.constant('baseServiceUrl', 'http://softuni-social-network.azurewebsites.net');
+app.constant('pageSize', 2);
+
+app.config(function ($routeProvider) {
+
+    $routeProvider.when('/login', {
+        templateUrl: 'templates/login.html',
+        controller: 'LoginController'
+    });
+
+    $routeProvider.when('/register', {
+        templateUrl: 'templates/register.html',
+        controller: 'RegisterController'
+    });
+
+    $routeProvider.otherwise(
+        { redirectTo: '/' }
+    );
+
+});
+
+app.run(function ($rootScope, $location, authService) {
+  $rootScope.$on('$locationChangeStart', function (event) {
+    if ($location.path().indexOf("/user/") != -1 && !authService.isLoggedIn()) {
+      // Authorization check: anonymous site visitors cannot access user routes
+      $location.path("/");
+    }
+  });
+});
