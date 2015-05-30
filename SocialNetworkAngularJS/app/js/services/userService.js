a466c66957ad363035ae4b71e96a5128b8679b40
Tzezo <tzvetomir.krastev@gmail.com>
2015-05-30 14:24:19 +0300

    user Service

--- SocialNetworkAngularJS/app/js/services/userService.js
+++ SocialNetworkAngularJS/app/js/services/userService.js
@@ -1,3 +1,18 @@
 'use strict';
 
-//TODO
+app.factory('userService', function($http, baseServiceUrl, authService) {
+    var service = {};
+
+    var userServiceUrl = baseServiceUrl + '/api/me';
+
+    service.ChangePassword = function(changePasswordData, success, error) {
+        $http.put(userServiceUrl + '/ChangePassword', changePasswordData, {
+                headers: authService.getAuthHeaders()
+            })
+            .success(function(data, status, headers, config) {
+                success(data);
+            }).error(error);
+    };
+
+    return service;
+});
