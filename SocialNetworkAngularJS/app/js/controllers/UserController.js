f7a328b7e63ae2dc1464256ee70cb73eff9716cc
Tzezo <tzvetomir.krastev@gmail.com>
2015-05-30 16:19:41 +0300

    Edit user profile

--- SocialNetworkAngularJS/app/js/controllers/UserController.js
+++ SocialNetworkAngularJS/app/js/controllers/UserController.js
@@ -3,6 +3,14 @@
 // The AppController holds the presentation logic for the entire app (common all screens)
 app.controller('UserController',
     function ($scope, $location, authService, notifyService, userService) {
+
+        var ClearData = function() {
+            $scope.loginData = "";
+            $scope.registerData = "";
+            $scope.userData = "";
+            $scope.passwordData = "";
+        };
+
         $scope.changePassword = function() {
           userService.ChangePassword($scope.userData,
               function(serverData) {
@@ -14,5 +22,18 @@ app.controller('UserController',
                   notifyService.showError("Unsuccessful Password Change!", serverError);
               });
         }
+
+        $scope.editUser = function() {
+           profileService.EditUserProfile($scope.userData,
+               function(serverData) {
+                   notifyService.showInfo("Successful Profile Edit!");
+                   ClearData();
+                   $location.path('/');
+               },
+               function(serverError) {
+                   notifyService.showError("Unsuccessful Profile Edit!", serverError);
+               });
+       };
+
     }
 );
