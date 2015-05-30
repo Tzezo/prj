f9847214b7b64c3f062c8dee210f45846ff4a028
Tzezo <tzvetomir.krastev@gmail.com>
2015-05-31 00:48:35 +0300

    Edit user profile

--- SocialNetworkAngularJS/app/js/controllers/UserController.js
+++ SocialNetworkAngularJS/app/js/controllers/UserController.js
@@ -11,6 +11,8 @@ app.controller('UserController',
             $scope.passwordData = "";
         };
 
+        $scope.userData = authService.getCurrentUser();
+
         $scope.changePassword = function() {
           userService.ChangePassword($scope.userData,
               function(serverData) {
@@ -24,16 +26,54 @@ app.controller('UserController',
         }
 
         $scope.editUser = function() {
-           profileService.EditUserProfile($scope.userData,
+           userService.EditUserProfile($scope.userData,
                function(serverData) {
                    notifyService.showInfo("Successful Profile Edit!");
                    ClearData();
+                   
                    $location.path('/');
                },
                function(serverError) {
                    notifyService.showError("Unsuccessful Profile Edit!", serverError);
                });
        };
+
+       $scope.addImage = function(inputID, thumbnailSelector) {
+          var PROFILE_IMG_LIMIT = 128;
+          var COVER_IMG_LIMIT = 1024;
+
+          // Reads the selected file and returns the data as a base64 encoded string
+          var input = document.getElementById(inputID);
+          var file = input.files[0],
+              fileSize = file.size / 1000,
+              reader;
+
+          if (file.type.match(/image\/(jpg|jpeg)/)) {
+              reader = new FileReader();
+              if (inputID == 'addAvatarImage') {
+                  if (fileSize <= PROFILE_IMG_LIMIT) {
+                      reader.onload = function(e) {
+                          $(thumbnailSelector).attr('src', e.target.result);
+                          $scope.userData.profileImageData = e.target.result;
+                      };
+                  } else {
+                      notifyService.showError("Image should be less than " + PROFILE_IMG_LIMIT + "kb");
+                  }
+              } else {
+                  if (fileSize <= COVER_IMG_LIMIT) {
+                      reader.onload = function(e) {
+                          $(thumbnailSelector).attr('src', e.target.result);
+                          $scope.userData.coverImageData = e.target.result;
+                      };
+                  } else {
+                      notifyService.showError("Image should be less than " + COVER_IMG_LIMIT + "kb");
+                  }
+              }
+              reader.readAsDataURL(file);
+          } else {
+              notifyService.showError("Wrong file format");
+          }
+      };
 
     }
 );
